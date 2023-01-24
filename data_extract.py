14ba4a7a7f84046ed9d1f60fa4541a4ef59e2a15
gsreeram1 <ganesh.sreeram@rwe.com>
2023-01-24 14:31:30 -0600

    updated data pulls to restructure data

--- data_extract.py
+++ data_extract.py
@@ -3,7 +3,8 @@ import urllib
 from sqlalchemy import create_engine
 import pandas as pd
 import dremio_caller as dc
-import datetime
+import datetime as dt
+from datetime import datetime, timedelta
 
 
 class dremio_data:
@@ -77,7 +78,7 @@ class ACDB:
         def get_ERCOT_load_actuals(self):
         
         
-                query = """select FCID, Convert(datetime,Cast(StartTime As datetime),108) As date, Value from ERCOT_Forecasts
+                query = """select FCID, Convert(datetime,Cast(StartTime As datetime),108) As StartTime, Value from ERCOT_Forecasts
                 WHERE FCID IN (110,111,112,113,114,115,116,117,119,120,121,122,123)"""
 
                 df = pd.read_sql_query(query,self.engine)
@@ -86,17 +87,21 @@ class ACDB:
 
                 df = df.convert_dtypes()
 
+                df = df.pivot_table('Value',['StartTime'],'FCID').reset_index()
+
                 return df
         
         def get_ERCOT_load_forecasts(self):
         
-                query = """select FCID, Convert(datetime,Cast(ERCOT_Forecasts.StartTime As datetime),108) As StartTime, Value from ERCOT_Forecasts WHERE FCID IN (9,10,11,12,13,14,15,16,17,18,19,20,21)"""
+                query = """select FCID, Convert(datetime,Cast(ERCOT_Forecasts.StartTime As datetime),108) As StartTime, Value from ERCOT_Forecasts WHERE FCID IN (9,10,11,12,13,14,15,16,17,18,19,21)"""
         
                 df = pd.read_sql_query(query,self.engine)
         
                 df['FCID'] = df['FCID'].replace([9,10,11,12,13,14,15,16,17,18,19,21],['North','South','West','Houston','Coast','East','FarWest','North','NorthCentral','SouthCentral','Southern','System'])
 
-                df = df.convert_dtypes() 
+                df = df.convert_dtypes()
+
+                df = df.pivot_table("Value",'StartTime','FCID').reset_index() 
         
                 return df
 
@@ -111,6 +116,8 @@ class ACDB:
                 df = pd.read_sql_query(query,self.engine)
 
                 df = df.convert_dtypes()
+
+                df = df.pivot_table('Value','StartTime','SettlementPointName').reset_index()
         
                 return df 
 
@@ -125,6 +132,10 @@ class ACDB:
                 df = pd.read_sql_query(query,self.engine)
 
                 df = df.convert_dtypes()
+
+                df = df.pivot_table('Value','StartTime','SettlementPointName').reset_index()
+
+                df = df.resample('H',on = 'StartTime').mean().reset_index()
         
                 return df
 
@@ -144,6 +155,8 @@ class ACDB:
                 df['FCID'] = df['FCID'].replace([32,33,35,36,38,39,41,42,44,45,108,109],['PANHANDLE','PANHANDLE','COASTAL','COASTAL','SOUTH','SOUTH','WEST','WEST','NORTH','NORTH','SYSTEM_WIDE','SYSTEM_WIDE'])
 
                 df = df.convert_dtypes()
+
+                df = df.pivot_table('Value',['StartTime','Type'],'FCID').reset_index()
         
                 return df
 
@@ -159,11 +172,13 @@ class ACDB:
         
                 df = pd.read_sql_query(query,self.engine)
 
-                dict = {22:'SYSTEM_WIDE',23:'PANHANDLE',24:'COASTAL',25:'SOUTH',26:'WEST',27:'NORTH',46:'SYSTEM_WIDE',106:'SYSTEM_WIDE'}
+                dict = {22:'SYSTEM_WIDE',23:'PANHANDLE',24:'COASTAL',25:'SOUTH',26:'WEST',27:'NORTH',46:'SYSTEM_WIDE_1',106:'SYSTEM_WIDE'}
 
                 df['FCID'] = df['FCID'].map(dict)
 
                 df = df.convert_dtypes()
+
+                df = df.pivot_table('Value',['StartTime','Type'],'FCID').reset_index()
         
                 return df
 
@@ -185,6 +200,8 @@ class ACDB:
                 df['FCID'] = df['FCID'].map(dict)
 
                 df = df.convert_dtypes()
+
+                df = df.pivot_table('Value','StartTime','FCID').reset_index()
         
                 return df
 
@@ -224,13 +241,22 @@ class ACDB:
 
                 df = df.convert_dtypes()
 
-                return df
+                data = pd.DataFrame()
+
+                data['date'] = df['TradeDate'].unique()
+
+                data['strip_date'] = data['date'].apply(lambda x: x.replace(day=1))
+                data['strip_date'] = data['strip_date'].apply(lambda x:x + timedelta(days=32)).apply(lambda x: x.replace(day=1)) #Change time delta to 64 to get 2 months ahead prompt+1
+
+                df_2 = data.merge(df,left_on = ['date','strip_date'], right_on = ['TradeDate','Strip'])
+
+                return df_2
 
 
-        def get_North_HR(self):
+        def get_North_HR(self): #Prompt -- XRW is the ATC HR
 
                 query = """(Select 'Current' As Lbl, * From
-                        (Select CurveID, CurveID, Convert(datetime,Cast(TradeDate As datetime),108) As TradeDate, Convert(datetime,Cast(Strip As datetime),108) As Strip, Convert(datetime,Cast(ExpirationDate As datetime),108) AS ExpirationDate, SettlementPrice, Hub, Commodity, Contract, ContractType, Exchange, Product
+                        (Select CurveID, Convert(datetime,Cast(TradeDate As datetime),108) As TradeDate, Convert(datetime,Cast(Strip As datetime),108) As Strip, Convert(datetime,Cast(ExpirationDate As datetime),108) AS ExpirationDate, SettlementPrice, Hub, Commodity, Contract, ContractType, Exchange, Product
                         From
                         (Select fp.CurveID, fp.TradeDate, fp.SettlementPrice, f.Strip, f.ExpirationDate, f.Hub, f.Commodity, f.Contract, f.ContractType, f.Exchange, f.Product
                         From assetcommercialus.dbo.ICE_ForwardPrices fp
@@ -249,19 +275,28 @@ class ACDB:
                         ) As fp2
 
                         Where fp2.TradeDate > '2017-01-01'
-                        And (fp2.Contract = 'YGV' Or fp2.Contract = 'XPR' Or fp2.Contract = 'XPS' Or fp2.Contract = 'XPT' Or fp2.Contract = 'XPU' Or fp2.Contract = 'XRW') 
+                        And (fp2.Contract = 'XPR') 
                         ) As tblHRercotnorth)"""
 
                 df = pd.read_sql_query(query,self.engine)
 
                 df = df.convert_dtypes()
 
-                return df
+                data = pd.DataFrame()
 
-        def get_West_HR(self):
+                data['date'] = df['TradeDate'].unique()
+
+                data['strip_date'] = data['date'].apply(lambda x: x.replace(day=1))
+                data['strip_date'] = data['strip_date'].apply(lambda x:x + timedelta(days=32)).apply(lambda x: x.replace(day=1)) #Change time delta to 64 to get 2 months ahead prompt+1
+
+                df_2 = data.merge(df,left_on = ['date','strip_date'], right_on = ['TradeDate','Strip'])
+
+                return df_2
+
+        def get_West_HR(self): #Prompt -- XS9 is the ATC HR and XQH is on-peak
 
                 query = """(Select 'Current' As Lbl, * From
-                        (Select CurveID, CurveID, Convert(datetime,Cast(TradeDate As datetime),108) As TradeDate, Convert(datetime,Cast(Strip As datetime),108) As Strip, Convert(datetime,Cast(ExpirationDate As datetime),108) AS ExpirationDate, SettlementPrice, Hub, Commodity, Contract, ContractType, Exchange, Product
+                        (Select CurveID, Convert(datetime,Cast(TradeDate As datetime),108) As TradeDate, Convert(datetime,Cast(Strip As datetime),108) As Strip, Convert(datetime,Cast(ExpirationDate As datetime),108) AS ExpirationDate, SettlementPrice, Hub, Commodity, Contract, ContractType, Exchange, Product
                         From
                         (Select fp.CurveID, fp.TradeDate, fp.SettlementPrice, f.Strip, f.ExpirationDate, f.Hub, f.Commodity, f.Contract, f.ContractType, f.Exchange, f.Product
                         From assetcommercialus.dbo.ICE_ForwardPrices fp
@@ -279,14 +314,23 @@ class ACDB:
                         On f.CurveID = fp.CurveID
                         ) As fp2
                         Where fp2.TradeDate > '2017-01-01'
-                        And (fp2.Contract = 'YGY' Or fp2.Contract = 'XQH' Or fp2.Contract = 'XQJ' Or fp2.Contract = 'XQK' Or fp2.Contract = 'XQI' Or fp2.Contract = 'XS9') 
+                        And (fp2.Contract = 'XQH') 
                         ) As tblHRercotwest)"""
 
                 df = pd.read_sql_query(query, self.engine)
 
                 df = df.convert_dtypes()
 
-                return df
+                data = pd.DataFrame()
+
+                data['date'] = df['TradeDate'].unique()
+
+                data['strip_date'] = data['date'].apply(lambda x: x.replace(day=1))
+                data['strip_date'] = data['strip_date'].apply(lambda x:x + timedelta(days=32)).apply(lambda x: x.replace(day=1)) #Change time delta to 64 to get 2 months ahead prompt+1
+
+                df_2 = data.merge(df,left_on = ['date','strip_date'], right_on = ['TradeDate','Strip'])                
+
+                return df_2
 
         
         def get_ICE_North_Prices(self):
@@ -312,7 +356,7 @@ class ACDB:
                         On f.CurveID = fp.CurveID
                         ) As fp2
                         Where fp2.TradeDate > '2017-01-01'
-                        And (fp2.Contract = 'END') 
+                        And (fp2.Contract = 'END' Or fp2.Contract = 'XRT' Or fp2.Contract = 'XPA' Or fp2.Contract = 'XPB' Or fp2.Contract = 'XPC' Or fp2.Contract = 'XPD') 
                         ) As tblphysercotnorth)) As tblhenryhub"""
 
                 df = pd.read_sql_query(query,self.engine)
@@ -323,7 +367,7 @@ class ACDB:
 
         def get_total_outages(self):
 
-                query = """(Select Dateadd(Hour,HourEnding-1,Convert(datetime,Cast(Date As datetime),108)) As date
+                query = """(Select Dateadd(Hour,HourEnding-1,Convert(datetime,Cast(Date As datetime),108)) As StartTime
                         , HourEnding As HE
                         , TotalResourceMWZoneSouth+TotalResourceMWZoneNorth+TotalResourceMWZoneWest+TotalResourceMWZoneHouston As TotResourceCapOut
                         , TotalIRRMWZoneSouth+TotalIRRMWZoneNorth+TotalIRRMWZoneWest+TotalIRRMWZoneHouston As TotRenewResourceCapOut
